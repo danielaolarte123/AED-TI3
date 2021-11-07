d6961d36222ed013526ba136272a999a2aea5095
Jacobo0312 <juanjacobogarcia4@gmail.com>
2021-11-07 10:33:01 -0600

    Init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# AED-T3
+# XXXX - Integrative Task 3
+
+This program reflects the functionality of basketball statistics used to search relevant data.
+
+## Tools:
+* Java.
+* Java FX.
+* Eclipse.
+* Visual Studio Code
+* IntelliJ IDEA
+
+## Participants:
+* Daniela Olarte
+* Santiago Arevalo
+* Jacobo Garcia
+* Alexander Sanchez
+
+## Summary: 
+
+FIBA is the regulatory body for basketball worldwide, given the recent avalanche of numbers of figures from the games has decided that the most relevant data of each of the professionals in the basketball. Patterns about the development of sport and more criteria to get an idea of where the sports is currently heading. In this program, 200,000 data about NBA players will be loaded with information such as name, last name, age, team, points, rebounds, assisst, robberies and blocks. 
+
+Moreover, you can find all documentation of this app [here](___).
---
+++ src/ui/Main.java
@@ -0,0 +1,50 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+	//private FIBAController fibaController;
+    //private FIBA fiba;
+
+    public Main() {
+    
+            //fiba = new FIBA();
+      
+        //fibaController=new FIBAController(fiba);
+    }
+
+
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		/*
+		//FXMLLoader fxmload = new FXMLLoader(getClass().getResource("Pane.fxml"));
+		fxmload.setController(fibaController);
+		Parent root=fxmload.load();
+		Scene scene = new Scene(root,700,500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setTitle("FIBA");
+		primaryStage.setResizable(false);
+
+		Image icon = new Image("resources/FIBA-logo.png");
+		primaryStage.getIcons().add(icon);
+		fibaController.loadMenu();
+		*/
+
+
+	}
+
+}
