931deeba25646d8ff4f273474ad8747304b72103
Jacobo0312 <juanjacobogarcia4@gmail.com>
2021-11-30 10:34:41 -0500

    Add new functions for gui

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,14 +1,15 @@
 package ui;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.layout.AnchorPane;
 import model.IcesiMap;
 
@@ -25,6 +26,9 @@ public class Controller {
     @FXML
     private ComboBox<String> finalPoint;
 
+    @FXML
+    private ListView<String> listView;
+
     public Controller(IcesiMap icesiMap) {
         this.icesiMap = icesiMap;
 
@@ -40,33 +44,43 @@ public class Controller {
         mainPane.getChildren().clear();
         mainPane.getChildren().addAll(pane);
 
-
         initPoint.getItems().addAll(icesiMap.getPlaces());
         finalPoint.getItems().addAll(icesiMap.getPlaces());
 
     }
 
     @FXML
-    public void findBestRoute(Event event) throws IOException {
+    public void findBestRoute(ActionEvent event) throws IOException {
 
-            loadRoute();
-        
+        loadRoute();
 
     }
 
     @FXML
-	public void loadRoute() throws IOException {
+    public void loadRoute() throws IOException {
 
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Route.fxml"));
-		fxmlLoader.setController(this);
-		Parent pane = fxmlLoader.load();
-        
-		mainPane.getChildren().clear();
-		mainPane.getChildren().addAll(pane);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Route.fxml"));
+        fxmlLoader.setController(this);
+        Parent pane = fxmlLoader.load();
 
-        //List view
+        mainPane.getChildren().clear();
+        mainPane.getChildren().addAll(pane);
 
+        // List view
+        //TODO add method of IcesiMap
+        listView.getItems().addAll("a", "a", "a", "a");
 
-	}
+    }
+
+    @FXML
+    public void changePoints(ActionEvent event) {
+
+        String aux = initPoint.getSelectionModel().getSelectedItem();
+
+        initPoint.getSelectionModel().select(finalPoint.getSelectionModel().getSelectedItem());
+
+        finalPoint.getSelectionModel().select(aux);
+
+    }
 
 }
